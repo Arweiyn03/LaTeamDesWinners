329d083110fa7d7639898333d91d9c760e7c78c6
Arweiyn03 <115633154+Arweiyn03@users.noreply.github.com>
2022-12-03 10:19:12 +0100

    Onglet
    Signed-off-by: Cassandra <115633154+Arweiyn03@users.noreply.github.com>

--- Interface.py
+++ Interface.py
@@ -3,16 +3,19 @@ import maya.cmds as cmds
 
 def createWindow():
     
+    #declaration de mes variables
     global slidernmbrchemins
     global slidertaillechemins
     global slidernombrebioms1
     global slidernombrebioms2
     global slidernombrebioms3
     global slidernombrebioms4
-        
+    
+    #creation de ma fenetre    
     win = cmds.window("World Edit")
     cmds.rowColumnLayout(height=160,width=400,nc=2,cw=[(1,200),(2,200)])
-        
+    
+    #creation de mes boutons    
     cmds.text(label="Nombre de chemins")
     slidernmbrchemins = cmds.intSliderGrp(min = 0, max = 100,sliderStep = 1,field = True, value = 20)
     
@@ -38,7 +41,7 @@ def createWindow():
     
     
     
-    
+#verifier que la fenetre exist, sinon la creer (pour eviter les problemes de   
 try : print (win)
 except : win = ""
 
@@ -50,7 +53,7 @@ if winExists :
 else :
         cmds.showWindow(createWindow());
         
-        
+#ca sert a r pour le moment, c'est juste pour me souvenir des commandes qui fond echo au random        
 from random import *
 import random
         
